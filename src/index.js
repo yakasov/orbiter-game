46ca442b08945ce492c5809c8e2147b541913913
James McDougall <james.mcdougall@theaccessgroup.com>
2023-07-11 08:11:13 +0100

    Formatting plus move prods to own class and improve(?) scaling

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-/*
-n: name
-a: amount
-t: translation (plural)
-ft: full translation (singular)
-cf: cost (first)
-cn: cost (now)
-cs: cost scale
-pf: produces base /s
-pn: produces now /s
-
-c1a: amount
-c1b: button
-c1p: producing
-*/
+class Prod {
+  constructor(name, trans, costStart, costScale, produces) {
+    this.n = name;
+    this.a = new Decimal(0);
+    this.t = `${trans.split(" ")[0].toLowerCase()}s`;
+    this.ft = trans;
+    this.cf = costStart;
+    this.cn = new Decimal(0);
+    this.cs = costScale;
+    this.pf = new Decimal(produces);
+    this.pn = new Decimal(0);
+  }
+}
 
 class Tier1 {
   constructor() {
     this.atomics = [
-      { n: "t1_c1", a: new Decimal(), t: "quarks", ft: "Quark Gatherer", cf: 10, cn: new Decimal(), cs: 2, pf: new Decimal(1), pn: new Decimal() },
-      { n: "t1_c2", a: new Decimal(), t: "hadrons", ft: "Hadron Hunters", cf: 100, cn: new Decimal(), cs: 2, pf: new Decimal(10), pn: new Decimal() },
+      new Prod("t1_c1", "Quark Gatherer", 10, 1.15, 2),
+      new Prod("t1_c2", "Hadron Hunter", 100, 1.15, 10),
     ];
     this.producing = 0;
   }
@@ -32,7 +30,7 @@ class Tier1 {
   }
 
   updateAtomicInternals(c) {
-    c.cn = c.cf + c.a ** c.cs; // cost scaling
+    c.cn = c.a.add(c.a.add(1).mul(c.cf)).sub(c.cf).pow(c.cs).toFixed(2); // cost scaling
     c.pn = c.pf.mul(c.a);
   }
 
@@ -51,18 +49,18 @@ class Tier1 {
     if (ela) {
       ela.innerText = `${c.a} ${c.t}`;
     }
-    
+
     if (elb) {
       elb.innerText = `Buy 1 ${c.ft} for ${c.cn.toString()}`;
     }
-    
+
     if (elp) {
       elp.innerText = `${c.pn.toString()} matter /s`;
     }
   }
 
   updateBalance() {
-    gl.ec.addToBalance(this.producing.div(10))   
+    gl.ec.addToBalance(this.producing.div(10));
   }
 
   updateLoop() {
@@ -70,7 +68,7 @@ class Tier1 {
     this.atomics.forEach((c) => {
       this.updateAtomicInternals(c);
       this.updateAtomicDisplays(c);
-      p = p.add(c.pn)
+      p = p.add(c.pn);
     });
     this.producing = p;
   }
@@ -78,7 +76,7 @@ class Tier1 {
 
 class Economy {
   constructor() {
-    this.balance = new Decimal(10);
+    this.balance = new Decimal(0);
   }
 
   addToBalance(a) {
@@ -90,15 +88,15 @@ class Economy {
   }
 
   displayBalance() {
-    const el = document.getElementById('mainBalance');
+    const el = document.getElementById("mainBalance");
     el.innerText = `You have ${this.balance.toFixed(2)} matter.`;
   }
 
   displayProducing() {
     const producing = gl.t1.producing;
 
-    const el = document.getElementById('producingBalance');
-    el.innerText = `producing ${producing} matter /s`
+    const el = document.getElementById("producingBalance");
+    el.innerText = `producing ${producing} matter /s`;
   }
 
   updateLoop() {
@@ -114,10 +112,22 @@ class GameLoop {
   }
 
   main() {
-    setInterval(() => { try { this.t1.updateLoop().bind(this) } catch {} }, 10);
-    setInterval(() => { try { this.ec.updateLoop().bind(this) } catch {} }, 10);
+    setInterval(() => {
+      try {
+        this.t1.updateLoop().bind(this);
+      } catch {}
+    }, 10);
+    setInterval(() => {
+      try {
+        this.ec.updateLoop().bind(this);
+      } catch {}
+    }, 10);
 
-    setInterval(() => { try { this.t1.updateBalance().bind(this) } catch {} }, 100);
+    setInterval(() => {
+      try {
+        this.t1.updateBalance().bind(this);
+      } catch {}
+    }, 100);
   }
 }
 
