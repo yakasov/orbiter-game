b309125f646a6c8792b11390584a11a34ae2890e
James McDougall <james.mcdougall@theaccessgroup.com>
2023-07-12 08:39:42 +0100

    Add upgrades (sorta)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 class Prod {
-  constructor(name, trans, costStart, costScale, produces) {
-    this.n = name;
+  constructor(id, trans, costStart, costScale, produces) {
+    this.n = id;
     this.a = new Decimal(0);
     this.t = `${trans.split(" ")[0].toLowerCase()}s`;
     this.ft = trans;
@@ -12,37 +12,78 @@ class Prod {
   }
 }
 
+class Upgrade {
+  constructor(id, np, trans, cost, bonus) {
+    this.n = id;
+    this.np = np;
+    this.b = false;
+    this.a = false;
+    this.ft = trans;
+    this.c = cost;
+    this.p = bonus;
+  }
+}
+
 class Tier1 {
   constructor() {
-    this.atomics = [
-      new Prod("t1_c1", "Quark Gatherer", 10, 1.15, 2),
-      new Prod("t1_c2", "Hadron Hunter", 100, 1.15, 10),
+    this.prods = [
+      new Prod("t1_c1p", "Quark Gatherer", 10, 1.15, 2),
+      new Prod("t1_c2p", "Hadron Hunter", 100, 1.15, 10),
+    ];
+    this.upgrades = [
+      new Upgrade("t1_u1", "t1_c1p", "Gatherer Magnets", 20, (p) => {
+        return p * 2;
+      }),
     ];
     this.producing = 0;
   }
 
-  buyAtomic(n) {
-    var atomic = this.atomics[n];
-    if (gl.ec.balance.gte(atomic.cn)) {
-      atomic.a = atomic.a.plus(1);
-      gl.ec.removeFromBalance(atomic.cn);
+  buyProd(n) {
+    var p = this.prods[n];
+    if (gl.ec.balance.gte(p.cn)) {
+      p.a = p.a.plus(1);
+      gl.ec.removeFromBalance(p.cn);
+    }
+  }
+
+  buyUpgrade(n) {
+    var u = this.upgrades[n];
+    if (gl.ec.balance.gte(u.c)) {
+      u.b = true;
+      gl.ec.removeFromBalance(u.c);
     }
   }
 
   updateAtomicInternals(c) {
-    c.cn = c.a.add(c.a.add(1).mul(c.cf)).sub(c.cf).pow(c.cs).toFixed(2); // cost scaling
+    c.cn = c.a
+      .add(c.a.add(1).mul(c.cf))
+      .sub(c.cf)
+      .pow(c.cs)
+      .add(c.cf)
+      .toFixed(2); // cost scaling
+    // currently: (n + ((n + 1) * cf) - cf) ^ cs + cf
+    // for t1_c1: (n + ((n + 1) * 10) - 10) ^ 1.15 + 10
+    // prices   : 10.00, 25.76, 44.98, 65.76
     c.pn = c.pf.mul(c.a);
+
+    this.upgrades
+      .filter((u) => u.np === c.n && u.b && !u.a)
+      .forEach((u) => {
+        c.pf = u.p(c.pf);
+        u.a = true;
+      });
   }
 
   updateAtomicDisplays(c) {
     const ela = document.getElementById(`${c.n}a`); // amount eg '12 quarks'
     const elb = document.getElementById(`${c.n}b`); // button string
-    const elg = document.getElementById(`${c.n}g`); // group for display: none
+    const elg = document.getElementById(`${c.n}g`); // group for visibility
     const elp = document.getElementById(`${c.n}p`); // producing eg '50 matter /s'
 
     if (elg) {
-      if (elg.style.display === "none" && gl.ec.balance.gte(c.cf)) {
-        elg.style.display = "";
+      if (elg.classList.contains("hidden") && gl.ec.balance.gte(c.cf)) {
+        elg.classList.remove("hidden");
+        elg.classList.add("fade-in");
       }
     }
 
@@ -59,24 +100,39 @@ class Tier1 {
     }
   }
 
+  updateAtomicUpgradeDisplays(u) {
+    const elg = document.getElementById(`${u.n}g`); // group for visibility
+    console.warn(elg);
+
+    if (elg) {
+      if (elg.classList.contains("hidden") && gl.ec.balance.gte(u.c.div(2))) {
+        elg.classList.remove("hidden");
+        elg.classList.add("fade-in");
+      }
+    }
+  }
+
   updateBalance() {
     gl.ec.addToBalance(this.producing.div(10));
   }
 
   updateLoop() {
     var p = new Decimal(0);
-    this.atomics.forEach((c) => {
+    this.prods.forEach((c) => {
       this.updateAtomicInternals(c);
       this.updateAtomicDisplays(c);
       p = p.add(c.pn);
     });
+    // this.upgrades.forEach((u) => {
+    //   this.updateAtomicUpgradeDisplays(u);
+    // });
     this.producing = p;
   }
 }
 
 class Economy {
   constructor() {
-    this.balance = new Decimal(0);
+    this.balance = new Decimal(10);
   }
 
   addToBalance(a) {
